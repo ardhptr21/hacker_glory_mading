23407680c03dec9f16759f2931c1d4e23e3845b0
Ardhi Putra <ardhiputrapradana21@gmail.com>
2023-12-17 20:46:09 +0700

    chore: set user seeder and install laravel excel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,16 +7,11 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    $this->call(UserSeeder::class);
+  }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    User::create([
+      'name' => 'Admin Ganteng',
+      'username' => 'admin',
+      'email' => 'admin@hackerglory.id',
+      'role' => 'admin',
+      'password' => 'secret123'
+    ]);
+
+    User::create([
+      'name' => 'Jono Doe',
+      'username' => 'jono',
+      'email' => 'jonodoe@hackerglory.id',
+      'role' => 'penulis',
+      'password' => 'secret123'
+    ]);
+
+    User::create([
+      'name' => 'Joni Doe',
+      'username' => 'joni',
+      'email' => 'jonidoe@hackerglory.id',
+      'role' => 'penulis',
+      'password' => 'secret123'
+    ]);
+
+    User::factory(30)->create();
+  }
+}
